c6402b1dcab93a8b2599d9e8b5c11cd1f1abbc21
Arnold-Dsouza <arnolddsouza1999@gmail.com>
2025-05-18 19:29:20 +0200

    Remove unused configuration file and update project name from "flashcard" to "flashlearn". Revise package dependencies and scripts for improved functionality, including TypeScript support and enhanced development tools. Update PostCSS configuration to utilize Tailwind CSS and autoprefixer.

---
+++ src/app/learn/page.tsx
@@ -0,0 +1,10 @@
+
+import { FlashcardViewer } from '@/components/flashcard-viewer';
+
+export default function LearnPage() {
+  return (
+    <div className="flex-grow flex flex-col">
+      <FlashcardViewer />
+    </div>
+  );
+}
